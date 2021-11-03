76da03aac2a1644b15335bd2addc29745fd9e461
Ben <van.citters.b@gmail.com>
2021-11-03 13:30:25 -0500

    initial commit

---
+++ progs/copyDat.bat
@@ -0,0 +1,4 @@
+copy %CD%\..\progs.dat "c:\Users\%USERNAME%\Saved Games\Nightdive Studios\Quake\mpmod"\progs.dat
+copy %CD%\..\progs.dat "c:\Users\%USERNAME%\Saved Games\Nightdive Studios\Quake\Id1"\progs.dat
+timeout /T 2
+"c:\Program Files (x86)\Steam\steamapps\common\Quake\rerelease\Quake_x64_steam.exe"
